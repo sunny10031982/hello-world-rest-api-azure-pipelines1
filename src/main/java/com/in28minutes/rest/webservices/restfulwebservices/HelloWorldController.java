1bd2d5a00b9f92470464c43126e8c8a76b9fcda8
Ravi <ravin@anyaconsultancy.com>
2020-10-06 17:18:34 +0100

    d

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,5 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,8 +27,6 @@ public class HelloWorldController {
 	
 	@RequestMapping("/generateAuditFile")
     public String generateAuditFileController(){
-
 		return "Hello generateAuditFile";
-
     }
 }
